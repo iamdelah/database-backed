bd55762b89b2acf1b9552bedeb217f2e007e7d10
iamdelah <dherlali888@gmail.com>
2023-10-10 21:05:29 +0000

    got migrations working. no visbile changes

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import login_required, login_user, LoginManager, logout_user, UserMixin, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
@@ -21,6 +22,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.secret_key = "my first flask app"
 login_manager = LoginManager()
